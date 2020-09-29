eb22daec316123342f667f2ffb267114775101c6
justin <justin.hu@tal.com>
2020-09-29 20:22:08 +0800

    all

---
+++ next.js-canary/examples/with-universal-configuration-build-time/pages/index.js
@@ -0,0 +1,6 @@
+export default () => (
+  <div>
+    <p>Environment variable process.env.TEST is "{process.env.TEST}"</p>
+    <p>Custom environment variables process.env.BACKEND_URL is "{process.env.BACKEND_URL}"</p>
+  </div>
+)
